08887fd0ed2e375fad1e0b6d041cc57eb4c1af6e
Rich Morgan <ionux@users.noreply.github.com>
2014-03-04 13:32:19 -0500

    Updated copyright

--- BitPay/Properties/AssemblyInfo.cs
+++ BitPay/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BitPay")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
