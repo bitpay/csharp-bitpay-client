3084a9159ab0f2b639cfb5b2eebac81ff31e9563
ajp8164 <kittyhawk1>
2016-02-11 15:50:28 -0500

    Add missing Buyer class.

---
+++ BitPay/Buyer.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BitPayAPI
+{
+    /// <summary>
+    /// Provides an interface to a buyer.
+    /// </summary>
+    public class Buyer
+    {
+        public Buyer() { }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "address1")]
+        public string Address1 { get; set; }
+
+        [JsonProperty(PropertyName = "address2")]
+        public string Address2 { get; set; }
+
+        [JsonProperty(PropertyName = "city")]
+        public string City { get; set; }
+
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        [JsonProperty(PropertyName = "zip")]
+        public string zip { get; set; }
+
+        [JsonProperty(PropertyName = "country")]
+        public string country { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string email { get; set; }
+
+        [JsonProperty(PropertyName = "phone")]
+        public string phone { get; set; }
+    }
+}
