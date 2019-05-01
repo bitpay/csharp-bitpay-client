2ebdc244d54a036ac649a40d058b887d2bb47bd4
Antonio Buedo <anbugal@users.noreply.github.com>
2019-05-01 16:12:32 +0200

    #v2-0 - Detached config from library (#39)
    Config extracted into env file for an easy configuration of the service

---
+++ BitPay/Exceptions/ConfigNotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BitPayAPI.Exceptions
+{
+    public class ConfigNotFoundException : BitPayException
+    {
+        private const string BitPayMessage = "An unexpected error occured while trying to retrieve the configuration";
+        private readonly string _bitpayCode = "BITPAY-CONFIG-READ";
+
+        public ConfigNotFoundException() : base(BitPayMessage)
+        {
+            BitpayCode = _bitpayCode;
+        }
+
+        public ConfigNotFoundException(Exception ex) : base(BitPayMessage, ex)
+        {
+            BitpayCode = _bitpayCode;
+        }
+
+        public ConfigNotFoundException(string bitpayCode, string message) : base(bitpayCode, message)
+        {
+        }
+
+        public ConfigNotFoundException(string bitpayCode, string message, Exception cause) : base(bitpayCode, message,
+            cause)
+        {
+        }
+    }
+}
