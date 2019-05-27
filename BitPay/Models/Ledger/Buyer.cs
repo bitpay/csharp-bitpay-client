68f0300904f58eb0ebf9afacf69c7cedf89009cb
Antonio Buedo <sales-engineering@bitpay.com>
2019-05-27 16:44:42 +0200

    - Ledger model refactored

--- BitPay/Models/Ledger/Buyer.cs
+++ BitPay/Models/Ledger/Buyer.cs
@@ -30,29 +30,5 @@ namespace BitPayAPI.Models.Ledger
         [JsonProperty(PropertyName = "buyerZip")] public string Zip { get; set; }
         
         [JsonProperty(PropertyName = "buyerCity")] public string City { get; set; }
-
-        
-        
-        
-//        [JsonProperty(PropertyName = "name")] public string Name { get; set; }
-//
-//        [JsonProperty(PropertyName = "address1")]
-//        public string Address1 { get; set; }
-//
-//        [JsonProperty(PropertyName = "address2")]
-//        public string Address2 { get; set; }
-//
-//        [JsonProperty(PropertyName = "city")] public string City { get; set; }
-//
-//        [JsonProperty(PropertyName = "state")] public string State { get; set; }
-//
-//        [JsonProperty(PropertyName = "zip")] public string zip { get; set; }
-//
-//        [JsonProperty(PropertyName = "country")]
-//        public string country { get; set; }
-//
-//        [JsonProperty(PropertyName = "email")] public string email { get; set; }
-//
-//        [JsonProperty(PropertyName = "phone")] public string phone { get; set; }
     }
 }
